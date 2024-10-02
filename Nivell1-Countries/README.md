58156669470e18cdd4ee117a4f744df583eda595
Pia <piamunozt@gmail.com>
2024-10-02 13:48:46 +0200

    Collections nivell1 countries exercise

---
+++ Nivell1-Countries/README.md
@@ -0,0 +1,31 @@
+# IT-Academy-S1.03-Java Collections-Nivel1
+
+### Exercisi 3: Countries
+
+Donat el fitxer countries.txt que conté països i capitals. El programa ha de llegir el fitxer i guardar les dades en un HashMap<String, String>. El programa demana el nom de l’usuari/ària, i després ha de mostrar un país de forma aleatòria, guardat en el HashMap. Es tracta que l’usuari/ària ha d’escriure el nom de la capital del país en qüestió. Si l’encerta se li suma un punt. Aquesta acció es repeteix 10 vegades. Un cop demanades les capitals de 10 països de forma aleatòria, llavors s’ha de guardar en un fitxer anomenat classificacio.txt, el nom de l’usuari/ària i la seva puntuació.
+
+### Tecnologies Utilitzades
+
+Java
+
+### Requisits
+
+Java 23.0
+
+### Instal·lació
+
+Clona aquest repositori: -> git clone  
+Accedeix al directori del projecte:   cd Nivell1-Countries
+
+### Contribucions
+
+Les contribucions són benvingudes! Per favor, segueix els següents passos per a contribuir:  
+
+Fes un fork del repositori  
+Crea una nova branca:  git checkout -b feature/NovaFuncionalitat  
+Fes els teus canvis i commiteja'ls: git commit -m 'Afegeix Nova Funcionalitat'  
+Puja els canvis a la teva branca: git push origin feature/NovaFuncionalitat  
+Fes un pull request
+
+### Notes Exercisi 3
+
---
+++ Nivell1-Countries/src/App.java
@@ -0,0 +1,99 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+    final static String COUNTRIES_FILEPATH = "./src/countries.txt"; 
+    final static String SCORE_FILEPATH = "./src/scores.txt"; 
+    public static void main(String[] args) throws Exception {
+
+        Map<String, String> mapFromFile = HashMapFromTextFile(); 
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user name
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+
+        // Get 10 random countries from countries.txt
+        String[] countries = getRandomKey(mapFromFile);
+
+        // Repeat 10 times:
+        int score = 0;
+        for (int i = 0; i < 10; i++) {
+
+            // Get capital from country i
+            String country = countries[i];
+            String capital = mapFromFile.get(country);
+
+            // Ask the user for the capital
+            System.out.println(name + ", which is the capital of " + country);  
+            String capitalAttempt = sc.nextLine();
+    
+            // Check if the answer is correct and add 1 point in this case
+            if((capitalAttempt != null && capital != null && capitalAttempt.equals(capital))) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Meeeec! It was " + capital);
+            }
+        }
+        System.out.println("Thank you for participating!");
+    
+        // Create scores file if does not exist and add new line with user name and final score
+        Path path = Paths.get(SCORE_FILEPATH);
+        try {
+            appendToFile(path, name + " : " + score + System.lineSeparator());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static Map<String, String> HashMapFromTextFile() { 
+  
+        Map<String, String> map = new HashMap<>(); 
+  
+        try {
+            List<String> list = Files.readAllLines(Paths.get(COUNTRIES_FILEPATH));
+            for (String line : list) {
+                String[] parts = line.split(" "); 
+                String country = parts[0].trim(); 
+                String capital = parts[1].trim(); 
+                if (!country.equals("") && !capital.equals("")) {
+                    map.put(country, capital); 
+                }
+            }
+        } 
+        catch (IOException e) { 
+            System.out.println(e.getMessage());
+        } 
+        return map; 
+    }
+
+    private static void appendToFile(Path path, String content) throws IOException {
+        Files.write(path, content.getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND);
+
+    }
+
+    public static String[] getRandomKey(Map<String, String> map) {
+        ArrayList<String> keyList = new ArrayList<>(map.keySet());
+        Random random = new Random();
+        String array[] = new String[10];
+
+        for (int i = 0; i < 10; i++) {
+            array[i] = keyList.get(random.nextInt(keyList.size()));
+        }
+        return array;
+    }
+}
